574d634e67f72b426b7c8f15bd241e2e2e6de624
Florian Pritz <bluewind@xinu.at>
2017-02-26 20:45:25 +0100

    initial commit
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::BorgRestore - It's new $module
+
+# SYNOPSIS
+
+    use App::BorgRestore;
+
+# DESCRIPTION
+
+App::BorgRestore is ...
+
+# LICENSE
+
+Copyright (C) Florian Pritz.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Florian Pritz <bluewind@xinu.at>
---
+++ lib/App/BorgRestore.pm
@@ -0,0 +1,39 @@
+package App::BorgRestore;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::BorgRestore - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::BorgRestore;
+
+=head1 DESCRIPTION
+
+App::BorgRestore is ...
+
+=head1 LICENSE
+
+Copyright (C) Florian Pritz.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Florian Pritz E<lt>bluewind@xinu.atE<gt>
+
+=cut
+
