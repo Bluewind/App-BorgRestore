5b8a11a94c506c0bb8bad2163094686b2710b34f
Florian Pritz <bluewind@xinu.at>
2017-04-10 23:16:49 +0200

    Use Function::Parameters in Helper package
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- lib/App/BorgRestore/Helper.pm
+++ lib/App/BorgRestore/Helper.pm
@@ -3,24 +3,19 @@ use v5.10;
 use strict;
 use warnings;
 
+use Function::Parameters;
 use POSIX ();
 
-sub untaint {
-	my $data = shift;
-	my $regex = shift;
-
+fun untaint($data, $regex) {
 	$data =~ m/^($regex)$/ or die "Failed to untaint: $data";
 	return $1;
 }
 
-sub untaint_archive_name {
-	my $archive = shift;
+fun untaint_archive_name($archive) {
 	return untaint($archive, qr([a-zA-Z0-9-:+\.]+));
 }
 
-sub format_timestamp {
-	my $timestamp = shift;
-
+fun format_timestamp($timestamp) {
 	return POSIX::strftime "%a. %F %H:%M:%S %z", localtime $timestamp;
 }
 
