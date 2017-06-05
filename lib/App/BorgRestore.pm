ef8c42d5140a28313821ad9ef46976ca424e43ec
Florian Pritz <bluewind@xinu.at>
2017-06-06 00:04:42 +0200

    Move license documentation to end of file
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- lib/App/BorgRestore.pm
+++ lib/App/BorgRestore.pm
@@ -60,28 +60,13 @@ usage.
 
 This package uses L<Log::Any> for logging.
 
-=head1 LICENSE
 
-Copyright (C) 2016-2017  Florian Pritz E<lt>bluewind@xinu.atE<gt>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-See LICENSE for the full license text.
 
-=head1 AUTHOR
 
-Florian Pritz E<lt>bluewind@xinu.atE<gt>
 
 =cut
 
@@ -395,3 +380,28 @@ method update_cache() {
 
 1;
 __END__
+
+=head1 LICENSE
+
+Copyright (C) 2016-2017  Florian Pritz E<lt>bluewind@xinu.atE<gt>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+See LICENSE for the full license text.
+
+=head1 AUTHOR
+
+Florian Pritz E<lt>bluewind@xinu.atE<gt>
+
+=cut
